847be8bb97202810b428f46fc29de8a713930444
HelderF2821 <helder.ferreira2821@gmail.com>
2023-03-14 10:52:56 +0100

    Connexion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './Login';
+import Connexion from './Connexion';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Login/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Connexion/> 
       </header>
     </div>
   );
