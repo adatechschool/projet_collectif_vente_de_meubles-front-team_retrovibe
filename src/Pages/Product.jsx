cd62bd687420814957cdea534b4d0a23a47f3e25
HelderF2821 <helder.ferreira2821@gmail.com>
2023-03-16 16:07:18 +0100

    product changement de taille

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -5,22 +5,22 @@ import BtnCart from '../Composants/BtnCart';
 
 function Product() {
     return (
-        <div class="flex flex-row justify-center items-cente gap-5 mt-16">
+        <div class="flex flex-row justify-center items-cente gap-9 mt-16">
             <div class="w-2/5">
                 <img class="rounded-lg " src="https://cdn1.vente-unique.com/thumbnails/product/444/444517/product_raw/xs/fauteuil_9488677.jpg" alt="" />
             </div>
             <div class="flex flex-col justify-between">
                 <div>
-                    <img class="w-32 h-32 rounded-lg" src="https://cdn1.vente-unique.com/thumbnails/product/132/132613/product_raw/xs/fauteuil_492067.jpg" alt="" />
+                    <img class="w-40 h-40 rounded-lg" src="https://cdn1.vente-unique.com/thumbnails/product/132/132613/product_raw/xs/fauteuil_492067.jpg" alt="" />
                 </div>
                 <div>
-                    <img class="w-32 h-32   rounded-lg" src="https://www.azurahomedesign.com/77233/fauteuil-pivotant-jazz.jpg" alt="" />
+                    <img class="w-40 h-40   rounded-lg" src="https://www.azurahomedesign.com/77233/fauteuil-pivotant-jazz.jpg" alt="" />
                 </div>
                 <div>
-                    <img class="w-32 h-32  rounded-lg" src="https://www.cdiscount.com/pdt2/9/7/5/1/700x700/bai3584179057975/rw/fauteuil-coquillage-callista-velours-bleu-pieds.jpg" alt="" />
+                    <img class="w-40 h-40  rounded-lg" src="https://www.cdiscount.com/pdt2/9/7/5/1/700x700/bai3584179057975/rw/fauteuil-coquillage-callista-velours-bleu-pieds.jpg" alt="" />
                 </div>
                 <div>
-                    <img class="w-32 h-32  rounded-lg" src="https://cdn1.vente-unique.com/thumbnails/product/122/122449/product_raw/xs/fauteuil_488211.jpg" alt="" />
+                    <img class="w-40 h-40  rounded-lg" src="https://cdn1.vente-unique.com/thumbnails/product/122/122449/product_raw/xs/fauteuil_488211.jpg" alt="" />
                 </div>
             </div>
             <div class="shadow-lg ml-10 pl-10 pt-10 pr-10">
