0564ed30c3c51b44673a273410edf7d4acea28cc
pierre merlaud <pmerlaud007@gmail.com>
2023-03-23 20:44:57 +0100

    panier final

---
+++ src/Composants/ClearCartButtonByOne.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import Croix from "../Pages/croix_supp.png";
+
+
+// SUPPRIMER UN ARTICLE A LA FOIS EN SUPPRIMANT DU LOCALSTORAGE UNE VALEUR DU TABLEAU CONTENANT LES ID DES MEUBLES.
+
+
+
+function ClearCartButtonByOne(props) {
+    const clearStorageByOne = () => {
+
+        // Récupérer les éléments du panier (un tableau avec l'ID de l'article) depuis le local storage 
+        const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+
+        /* via props, je recup d'une page à l'autre l'index du meuble dans mon tableau meubles de ma page cart. 
+        (voir le bouton avec indexMeuble={index})
+        Je supprime ce meuble de mon localeStorage via son index et splice.*/
+        cartItems.splice(props.indexMeuble, 1); 
+
+        // Stocker la nouvelle liste dans le local storage
+        localStorage.setItem('cart', JSON.stringify(cartItems));
+
+        // Afficher la liste des éléments du panier dans la console (désactiver le rafraichissement pour voir)
+        console.log('localstorageOneLess', JSON.parse(localStorage.getItem('cart')));
+
+        // je rafraichis ma page
+        window.location.reload(false);
+
+
+      
+
+
+        
+
+    }
+        return (
+            <button onClick={clearStorageByOne} className="font-bold tracking-wide mb-6 mr-10 text-white bg-white transition-all duration-1000 hover:bg-violet-700 focus:outline-none rounded-lg  sm:w-auto m-auto px-5 py-2.5 text-center my-2 " alt="bouton du caddie">
+                <img src={Croix} alt="supprimer" className="w-10 h-10 p-3 bg-violet-400 rounded-full"/>
+
+            </button>
+        
+          );
+        
+    }
+
+    // export const clearStorageByOne = (index) => {
+    //     const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+    //     console.log('cartItems', cartItems)
+    //     // console.log('index', index)
+
+    //     cartItems.splice(index, 1);
+
+    //     // Update localStorage with the modified cart
+    //     localStorage.setItem('cart', JSON.stringify(cartItems));
+        
+    //     // window.location.reload(false);
+        
+
+    // }
+    
+    export default ClearCartButtonByOne;
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,21 +1,30 @@
 /* page 5 de la maquette */
 import React, {useEffect, useState} from "react";
-import Croix from "./croix_supp.png";
 import axios from 'axios';
 import ClearCartButton from '../Composants/ClearCartButton';
+import ClearCartButtonByOne from '../Composants/ClearCartButtonByOne';
+import Croix from "./croix_supp.png";
+
+
+
 
 function Cart() {
     const [meubles, setMeubles] = useState([]);
 
     useEffect(() => {
         const loadMeubles = async () => {
+
+            // je récupère mon tableau contenant les ID des meubles cliqués de mon localStorage
         
             let meubleId = JSON.parse(localStorage.getItem('cart'));
     
-            console.log("meubleId", meubleId)
             let tmpMeubles = [];
+
+            // je passe les ID un par un à mon axios
             for(let i = 0; i < meubleId.length; i++) {
                 const response = await axios.get(`https://retrovibe.herokuapp.com/api/meubles/${meubleId[i]}`);
+
+                // je push mes objets meubles récupérés dans un nouveau tableau
                 tmpMeubles.push(response.data);
             }
             setMeubles(tmpMeubles);
@@ -24,21 +33,26 @@ function Cart() {
         
     }, []);
 
-    console.log("total", meubles)
-
+    // je calcule mon total prix des articles
     const total = meubles.reduce((acc, meuble) => acc + parseInt(meuble.prix), 0);
 
+    // petite fonction en ternaire pour afficher le nombre d'articles.
+    function getArticleString(meubles) {
+        const count = meubles.length;
+        return count === 0 ? "Votre panier est vide 😕" : `Votre panier contient ${count} article${count > 1 ? "s 😄" : " 😏"}`;
+      }
+      
 
     return (
 
     <div className="flex flex-row mx-60 py-20 relative">
         <div className="w-8/12 flex flex-col"> 
-            <h2 className="text-3xl text-candlelight-500 font-bold ml-24">Votre panier contient {meubles.length} articles:</h2>
+            <h2 className="text-3xl text-candlelight-500 font-bold ml-24">{getArticleString(meubles)}</h2>
         <div class="mt-2 ml-24"><ClearCartButton /></div>
             
 
-            {meubles.map((meuble, index) => ( 
-            <div className="w-3/4 flex flex-col p-4 ml-20" key={meuble.id}>
+            {meubles.map((meuble, index) => (
+            <div className="w-3/4 flex flex-col p-4 ml-20" key={meuble.id} id={meuble.id}>
                 <h2 className="text-xl text-violet-400 font-bold mb-4 ml-2">Article n°{index + 1}</h2>
                 <div className="flex items-center bg-white p-2 border-2 border-candlelight-500 rounded-md mb-4 shadow-lg h-48 justify-between">
                     <div className="flex flex-row ">
@@ -54,6 +68,9 @@ function Cart() {
                             </div>
                             <p class = "font-bold text-lg mt-7">{meuble.prix} <span className="font-normal">€TTC</span></p>
                         </div>
+                                <div>
+                                    <ClearCartButtonByOne indexMeuble={index}/>
+                                </div>
                     </div>
                     <div>
                         <img src={Croix} alt="supprimer" fill="white" className="w-10 h-10 p-3 bg-violet-400  hover:bg-violet-700 focus:outline-none hover:scale-110 transition duration-700 ease-in-out rounded-full border mr-10"/>
@@ -92,7 +109,7 @@ function Cart() {
                     <div className="flex justify-between">
                         <p className="font-bold tracking-wide text-lg">Total</p>
                         <div className="flex flex-row gap-3">
-                            <p className="font-extrabold tracking-wide ">{total}</p>
+                            <p className="font-extrabold tracking-wide ">{total + (meubles.length * 10)} </p>
                             <p className="tracking-wide font-normal">€TTC</p>
                         </div>
 
