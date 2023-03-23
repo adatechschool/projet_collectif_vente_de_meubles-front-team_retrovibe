4c2d9e4af6c76dea96f55a7b53106ffb59e14861
NolwenMaj <115166022+NolwenMaj@users.noreply.github.com>
2023-03-23 13:14:22 +0100

    ticket de caisse sticky

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -30,19 +30,17 @@ function Cart() {
 
 
     return (
-    <div className="flex flex-row justify-content-center">
-        <div className="w-8/12 flex-column"> 
-            <div className="flex flex-col mt-8">
-                <h2 className="text-3xl text-candlelight-500 font-bold mt-8 mb-2 ml-24">Votre panier contient {meubles.length} articles:</h2>
-                <div class="mt-2 ml-24"><ClearCartButton /></div>
-            </div>
+    <div className="relative mx-60 py-20 flex flex-row justify-content-center">
+        <div className="w-8/12 flex flex-col"> 
+                <h2 className="text-3xl text-candlelight-500 font-bold mt-8 mb-2">Votre panier contient {meubles.length} articles:</h2>
+                <div class="mt-2"><ClearCartButton /></div>
 
             {meubles.map((meuble, index) => (
-            <div className="w-3/4 flex flex-col p-4 ml-20" key={meuble.id}>
-                <h2 className="text-xl text-violet-400 font-bold mb-4 ml-2">Article n°{index + 1}</h2>
+            <div className="w-3/4 flex flex-col py-4" key={meuble.id}>
+                <h2 className="text-xl text-violet-400 font-bold mb-4">Article n°{index + 1}</h2>
                 <div className="flex items-center bg-white p-4 border-2 border-candlelight-500 rounded-md mb-4 shadow-lg h-48 ">
-                    <div className="flex flex-row">
-                        <div className="w-1/5 h-4/6 rounded-full overflow-hidden mr-4 mt-2">
+                    <div className="flex flex-row justify-evenly">
+                        <div className="w-1/5 h-4/6 rounded-full overflow-hidden">
                             <img src={meuble.photo_1} alt="Visuel principal du produit" />
                         </div>
                         <div class = "flex flex-col">
@@ -50,7 +48,7 @@ function Cart() {
                             <div class = "flex flew-row">
                                 <p className="mb-2">{meuble.type}</p>
                                 <div>
-                                    <img src={Croix} alt="supprimer" className="w-10 h-10 ml-64 p-3 bg-violet-400 rounded-full"/>
+                                    <img src={Croix} alt="supprimer" className="w-10 h-10 p-3 bg-violet-400 rounded-full"/>
                                 </div>
                             </div>
                             <p class = "font-bold">{meuble.prix}</p>
@@ -61,10 +59,10 @@ function Cart() {
             ))}  
         </div>
 
-        <div className="w-4/12 p-4 flex flex-col mt-16 fixed right-20">
-            <div className="bg-white p-4 rounded-md mb-4 border-2 border-violet-400 shadow-lg ">
-                <p className="font-bold ml-4 mt-4 text-2xl text-center">Récapitulatif de commande</p>
-                <div className="flex flex-col mt-8 ml-8 mr-8 mb-32">
+        <div className="relative">
+            <div className="bg-white rounded-md p-4 border-2 border-violet-400 shadow-lg sticky top-40 bottom-40 ">
+                <p className="text-violet-700 font-bold ml-4 mt-4 text-2xl text-center">Récapitulatif de commande</p>
+                <div className="flex flex-col m-8">
                 {meubles.map((meuble, index) => (
                     <div className="flex justify-between">
                         <p className="tracking-wide">Article n°{index + 1}</p>
@@ -79,10 +77,10 @@ function Cart() {
                         <p className="font-bold tracking-wide">TTC</p>
                     </div>
                     <div class="content-none h-0.5 bg-black inset-y-0 mt-8 mb-8 ml-14 mr-14"/>
-                    <div className="flex justify-between">
-                        <p className="font-bold tracking-wide">Total</p>
-                        <p className="font-bold tracking-wide ml-36">{total} $</p>
-                        <p className="font-bold tracking-wide">TTC</p>
+                    <div className="text-violet-700 font-bold flex justify-between text-2xl">
+                        <p className=" tracking-wide">Total</p>
+                        <p className="tracking-wide ml-36">{total} $</p>
+                        <p className="tracking-wide">TTC</p>
                     </div>
                 </div>
             </div>
