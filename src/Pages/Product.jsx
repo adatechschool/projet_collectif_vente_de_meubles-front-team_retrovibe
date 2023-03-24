957afaccbdcb4d2caa49ae3a31ca5c6bb55c937a
IsabelleYi03 <114978047+IsabelleYi03@users.noreply.github.com>
2023-03-24 10:11:24 +0100

    product propre

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -11,6 +11,11 @@ function Product() {
 
     
      const [meuble, setMeuble] = useState({});
+     const [currentImage, setCurrentImage] = useState(meuble.photo_1);
+     const [currentPhoto, setCurrentPhoto] = useState(meuble.photo_2);
+
+
+     
 
     // je recupère mon id du meuble dans l'adresse de mon navigateur avec useParams
      const { id } = useParams();
@@ -32,68 +37,61 @@ function Product() {
     return (
 
         // PHOTO PRINCIPALE
-        <div className="flex flex-row justify-between my-16 mx-28 border">
-            <div>
-                <img className=" h-full rounded-lg border-4 border-candlelight-500" src={meuble.photo_1} alt="visuel principal de l'article" />
+        <div className="flex flex-row justify-center mt-28 gap-7 mb-44">
+            <div className="">
+                <img className="w-full rounded-lg cursor-pointer object-fill border-2 border-candlelight-500"
+                    onClick={() => setCurrentImage(meuble.photo_1)||setCurrentPhoto(meuble.photo_2)}
+                    src={currentImage||meuble.photo_1} alt="visuel principal de l'article"/>
             </div>
 
             {/* DIV PHOTOS */}
-            <div className="flex flex-col justify-start">
-                
-                {meuble.photo_2 !== null &&
-                    <div>
-                        <img className="w-32 h-32 rounded-lg" src={meuble.photo_2} alt="2e visuel de l'article" />
-                    </div>
-                    }
-                {meuble.photo_3 &&
-                    <div>
-                        <img className="w-32 h-32 rounded-lg" src={meuble.photo_3} alt="3e visuel de l'article" />
-                    </div>
-                    }
-                {meuble.photo_4 ?
-                    <div>
-                        <img className="w-32 h-32 rounded-lg" src={meuble.photo_4} alt="4e visuel de l'article" />
-                    </div>
-                    : null}
-                {meuble.photo_5 !== null &&
-                    <div>
-                        <img className="w-32 h-32 rounded-lg" src={meuble.photo_5} alt="5e visuel de l'article" />
-                    </div>
-                    }
+            <div className="flex flex-col justify-start gap-6  ">
+                    {meuble.photo_2 && (
+                <div onClick={() => setCurrentImage(meuble.photo_2) || setCurrentPhoto(meuble.photo_1)}>
+                    <img className="w-24 rounded-lg border-2 border-candlelight-500 cursor-pointer hover:scale-110 transition duration-700 ease-in-out "
+                    src={currentPhoto||meuble.photo_2}
+                    alt="2e visuel de l'article"/>
+                </div>
+                )}
+                {meuble.photo_3 && (
+                <div onClick={() => setCurrentImage(meuble.photo_3)}>
+                    <img className="w-24 rounded-lg border-2 border-candlelight-500 cursor-pointer hover:scale-110 transition duration-700 ease-in-out"
+                    src={meuble.photo_3}
+                    alt="3e visuel de l'article"/>
+                </div>
+                )}
+                {meuble.photo_4 && (
+                <div onClick={() => setCurrentImage(meuble.photo_4)}>
+                    <img className="w-24 rounded-lg border-2 border-candlelight-500 cursor-pointer hover:scale-110 transition duration-700 ease-in-out"
+                    src={meuble.photo_4}
+                    alt="4e visuel de l'article"/>
+                </div>
+                )}
+                {meuble.photo_5 && (
+                <div onClick={() => setCurrentImage(meuble.photo_5)}>
+                    <img className="w-24 rounded-lg border-2 border-candlelight-500 cursor-pointer hover:scale-110 transition duration-700 ease-in-out"
+                    src={meuble.photo_5}
+                    alt="5e visuel de l'article"/>
+                </div>
+                )}
             </div>
 
             {/* DIV INFOS */}
-            <div className="shadow-lg ml-10 pl-10 pt-10 pr-10">
+            <div className="shadow-lg ml-14  pt-8 px-8 tracking-wider">
                 <div>
-                    <h1 className="text-2xl pb-4">{meuble.nom}</h1>
-                    <div className="pb-14">
-                        <h1 className="text-4xl font-bold">{meuble.prix}<span className="text-xl font-normal ">€TCC</span></h1>
+                    <h1 className="text-xl pb-2 font-normal ">{meuble.nom}</h1>
+                    <div className="pb-10">
+                        <h1 className="text-4xl font-bold">{meuble.prix}<span className="text-xl font-normal"> €TCC</span></h1>
                     </div>
                 </div>
-                <div className="text-lg space-y-1 italic font-light">
-                    <h1>{meuble.type}</h1>
-                    {meuble.annee && <h1>{meuble.annee}</h1>}
-                    <h1>couleur(s) :
-                        <ul>
-                            <li>{meuble.couleur_1}</li>
-                            {meuble.couleur_2 && <li>{meuble.couleur_2}</li>}
-                        </ul>
-                    <h1>matiere(s) : 
-                        <ul>
-                            <li>{meuble.matiere_1}</li>
-                            {meuble.matiere_2 && <li>{meuble.matiere_2}</li>}
-                        </ul>
-                    </h1>
-                    </h1>
-                    <h1>dimensions : 
-                        <ul>
-                            <li>longueur : {meuble.longueur} cm</li>
-                            <li>largeur : {meuble.largeur} cm</li>
-                            <li>hauteur : {meuble.hauteur} cm</li>
-                        </ul>
-                    </h1>
+                <div className="text-xs space-y-1 italic font-light tracking-wider lowercase ">
+                    <h1>type : <span className="not-italic font-normal">{meuble.type}</span></h1>
+                    <h1>année : <span className="not-italic font-normal">{meuble.annee}</span></h1>
+                    <h1>couleur(s) : <span className="not-italic font-normal">{meuble.couleur_1}, {meuble.couleur_2}</span></h1>
+                    <h1>dimensions : <span className="not-italic font-normal">{meuble.longueur} x {meuble.largeur} x {meuble.hauteur}</span></h1>
+                    <h1>matière(s) : <span className="not-italic font-normal">{meuble.matiere_1}, {meuble.matiere_2}</span></h1>
                 </div>
-                <div className="pt-10">
+                <div className="justify-end pt-36 pl-48">
                 <BtnCart meubleId={meuble.id} />
                 </div>
             </div>
