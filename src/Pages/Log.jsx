86ef5ad174f357b64b112b5b57353e682fe9837c
NolwenMaj <115166022+NolwenMaj@users.noreply.github.com>
2023-03-31 14:17:16 +0200

    responsive ok

--- src/Pages/Log.jsx
+++ src/Pages/Log.jsx
@@ -7,11 +7,10 @@ import Connexion from "../Composants/Connexion";
 function Log() {
  
   return (  
-    <div className = "md:flex justify-center md:my-48">
-    <Connexion/>
-    <div className= "content-none w-0.5 bg-violet-400 inset-y-0 ">
-    </div>
-    <Inscription/>
+    <div className = "flex flex-col md:flex-row md:justify-center bg-violet-100 p-20 md:py-48 space-y-10 md:space-y-0 md:space-x-10">
+      <Connexion/>
+      <div className= "self-center content-none md:h-[200px] md:w-0.5 h-0.5 w-3/4 bg-violet-400"></div>
+      <Inscription/>
     </div>
   )
 }
